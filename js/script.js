100d9f5e3617a750d676768a54566d33134bbb5e
ericamendez <ecmendez25@gmail.com>
2022-05-02 14:09:32 -0400

    Artist is searchable and data is deconstructed in a 'populateDOM function'

--- js/script.js
+++ js/script.js
@@ -3,14 +3,42 @@ let infoSection = document.querySelector('#artistInfo')
 
 async function getArtist(e){
     e.preventDefault()
+    infoSection.innerHTML = ''
     let inputArtist = document.querySelector('#artist').value
 
     let response = await fetch(`https://www.theaudiodb.com/api/v1/json/2/search.php?s=${inputArtist}`)
     let data = await response.json()
-
-    let img = document.createElement('img')
-    img.src = 
+    
+    populateDOM(data.artists[0])
+    
     console.log(data)
+}
+
+function populateDOM({
+    idArtist,
+    strArtist, 
+    strArtistLogo,
+    strArtistThumb, 
+    strArtistFanart2,
+    strArtistFanart3,
+    strArtistFanart4,
+    strCountry,
+    strBiographyEN, 
+    strGenre, 
+    strWebsite
+}){
+    let imgLogo = document.createElement('img')
+    imgLogo.src = strArtistLogo
+    infoSection.appendChild(imgLogo)
+    
+    let img = document.createElement('img')
+    img.src = strArtistThumb
+    infoSection.appendChild(img)
+    
+    let p = document.createElement('p')
+    let bio = document.createTextNode(strBiographyEN)
+    p.appendChild(bio)
+    infoSection.appendChild(p)
 }
 
 submit.addEventListener('click', getArtist)
