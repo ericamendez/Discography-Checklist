8289eb2a4c210733d1df5bdd00e7bffdcd5dd5bb
ericamendez <ecmendez25@gmail.com>
2022-05-18 09:37:07 -0400

    simple adds

--- index.js
+++ index.js
@@ -26,6 +26,14 @@ app.use(Cors())
 app.use(Helmet())
 app.use(Express.json())
 
+/**
+/app/v1 -> Post -> add a data to database
+/api/v1 -> Get -> get all the data from database
+/app/v1/id -> Get -> get a single item data by id from database
+/app/v1 -> Delete -> delete a data from database
+/app/v1 -> Put -> update a data from database
+*/
+
 const app = Express()
 
 const port = process.env.PORT || 3000
